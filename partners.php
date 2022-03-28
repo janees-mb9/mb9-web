0a7293f879c728bf5a7ab7b8d84911265316ecf1
Sri <srivishnu.k90@gmail.com>
2022-03-28 20:16:52 +0530

    mb9 site

---
+++ partners.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<title>Partners - MB9</title> 
+<link href="images/favicon.ico" rel="icon" type="image/x-icon" /> 
+<link href="images/favicon.ico" rel="SHORTCUT ICON" type="image/x-icon" /> 
+<meta name="keywords" content="" /> 
+<meta name="description" content="" /> 
+ 
+<link rel="stylesheet" href="css/reset.css?v=1" type="text/css" media="screen, projection" /> 
+<link rel="stylesheet" href="css/screen.css?v=1" type="text/css" media="screen, projection" /> 
+<link rel="stylesheet" href="css/print.css?v=1" type="text/css" media="print" /> 
+<!--[if lt IE 8]>
+<link rel="stylesheet" href="css/ie.css?v=1" type="text/css" media="screen, projection" />
+<![endif]--> 
+
+<link rel="stylesheet" href="css/style.css?v=1" type="text/css" media="all" /> 
+ 
+</head>
+
+<body class="page"> 
+<div class="outercontainer">
+ <div class="container">
+  <div class="header">
+    <div class="logo span-6">
+      <a href="."><img src="images/logo.gif" /></a>
+    </div>
+    <div class="rhs span-18 last">
+        <div class="toplinks">
+            <a href="contact.php">Contact</a>
+            <a href="sitemap.php">Site Map</a>
+        </div>
+        <!--div class="search">
+            <form method="get">
+                <input type="text" value="" name="keyword" ></input>
+                <input type="submit" value="Search" name="submit" ></input>
+            </form>
+        </div-->
+    </div>
+  </div>
+  <div class="menu clear">
+   <div class="menulinks">
+        <a href="products.php">Products</a>
+        <a href="services.php">Services</a>
+        <a href="partners.php">Partners</a>
+        <a href="about.php">About</a>
+   </div>
+   <div class="menuimg">
+     <img src="images/bg_inner.gif" />
+   </div>
+  </div>
+  <div class="content">
+
+   <div class="lhssidebar span-6 prepend-1">
+     <div class="graphics pull-1">
+        <img src="images/tmpimg.gif" />
+     </div>
+   </div>
+   <div class="innercontent span-16 last">
+     <div>
+        <h3>Partners</h3>
+        <div class="body">
+          <p>
+            If you are looking to partner us in your development needs, Please contact
+            us at <a href="mailto:contact@mb9inc.com">contact@mb9inc.com</a>. Alternatively,
+            <a href="contact.php">click here</a> to send us a message from the 
+            <a href="contact.php">contact form</a>.
+          </p>
+        </div>
+     </div>
+   </div>
+
+   <div class="footer clear">
+     <div class="links">
+        <a href="contact.php">Contact</a>
+        <!--a href="downloads.php">Downloads</a-->
+        <a href="sitemap.php">SiteMap</a>
+     </div>
+     <div class="copyright">
+        Copyright &copy; <b>MB9 Pte Ltd</b>. All Rights Reserved.
+     </div>
+   </div>
+
+  </div>
+</div>
+</body>
+</html>
---
+++ wp-login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+<head>
+	<title>MB9Inc Blog &rsaquo; Log In</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<link rel='stylesheet' href='http://mb9inc.com/wp-admin/css/login.css%3fver=20081210' type='text/css' media='all' />
+<link rel='stylesheet' href='http://mb9inc.com/wp-admin/css/colors-fresh.css%3fver=20081210' type='text/css' media='all' />
+</head>
+<body class="login">
+
+<div id="login"><h1><a href="http://wordpress.org/" title="Powered by WordPress">MB9Inc Blog</a></h1>
+
+<form name="loginform" id="loginform" action="http://mb9inc.com/wp-login.php" method="post">
+	<p>
+		<label>Username<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" tabindex="10" /></label>
+	</p>
+	<p>
+		<label>Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" tabindex="20" /></label>
+	</p>
+	<p class="forgetmenot"><label><input name="rememberme" type="checkbox" id="rememberme" value="forever" tabindex="90" /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" value="Log In" tabindex="100" />
+		<input type="hidden" name="redirect_to" value="http://mb9inc.com/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+<a href="http://mb9inc.com/wp-login.php?action=lostpassword" title="Password Lost and Found">Lost your password?</a>
+</p>
+
+</div>
+
+<p id="backtoblog"><a href="http://mb9inc.com/" title="Are you lost?">&larr; Back to MB9Inc Blog</a></p>
+
+<script type="text/javascript">
+try{document.getElementById('user_login').focus();}catch(e){}
+</script>
+</body>
+</html>
